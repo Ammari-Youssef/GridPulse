2471745e1e00809e434e00859721c00d5ba176c8
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-04-30 14:37:03 +0100

    Initial Commit

---
+++ src/test/java/com/youssef/GridPulse/GridPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.GridPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
