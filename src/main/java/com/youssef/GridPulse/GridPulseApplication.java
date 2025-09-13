7c55438deb413a7131f932067cc0fb16f5cdbe8f
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-09-13 18:56:22 +0100

    refactor(pom): remove dotenv dependency and related config
    Eliminated Dotenv library from project dependencies and startup logic.
    Spring Boot now handles environment variables natively via application.properties and Docker Compose.

--- src/main/java/com/youssef/GridPulse/GridPulseApplication.java
+++ src/main/java/com/youssef/GridPulse/GridPulseApplication.java
@@ -1,6 +1,5 @@
 package com.youssef.GridPulse;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,18 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GridPulseApplication {
 
 	public static void main(String[] args) {
-		// Load .env file
-		Dotenv dotenv = Dotenv.load();
-
-		// Set system properties
-		// Loop over all env variables and set them as system properties
-		dotenv.entries().forEach(entry -> {
-			if (entry.getValue() != null) {
-				System.setProperty(entry.getKey(), entry.getValue());
-			} else {
-				throw new RuntimeException("❌ Missing value for key: " + entry.getKey());
-			}
-		});
 		SpringApplication.run(GridPulseApplication.class, args);
 	}
 
