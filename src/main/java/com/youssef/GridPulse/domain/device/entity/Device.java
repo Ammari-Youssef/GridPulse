d7f9aa29978299d7e19cc9873affb618b97f3088
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-09-28 18:05:16 +0100

    feat(device): add Device and DeviceHistory entities with database changelogs migrations

---
+++ src/main/java/com/youssef/GridPulse/domain/device/entity/Device.java
@@ -0,0 +1,51 @@
+package com.youssef.GridPulse.domain.device.entity;
+
+import com.youssef.GridPulse.domain.base.BaseEntity;
+import com.youssef.GridPulse.domain.identity.user.entity.User;
+import com.youssef.GridPulse.domain.inverter.entity.Inverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@Entity
+public class Device extends BaseEntity {
+
+    private Float soc; // State of Charge (0.0 - 100.0)
+    private String soh; // e.g., "98%"
+    private String batteryChemistry;
+    private Integer cycles;
+    private Double gpsLat;
+    private Double gpsLong;
+    private Instant lastSeen;
+    private String name;
+    private Float powerDispatched; // Can be negative or positive
+    private String status;
+    private Float temperature;
+    private Float voltage;
+    private String model;
+    private String manufacturer;
+    private String softwareVersion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "operator_id", referencedColumnName = "id", nullable = false)
+    private User operator;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "inverter_id", referencedColumnName = "id", nullable = false)
+    private Inverter inverter;
+
+}
