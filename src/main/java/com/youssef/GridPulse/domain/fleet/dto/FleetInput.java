8a5ff100153e60b82d27a26e627553608eb933e3
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-10-16 17:26:27 +0100

    feat(fleet): introduce Fleet input and mapper

---
+++ src/main/java/com/youssef/GridPulse/domain/fleet/dto/FleetInput.java
@@ -0,0 +1,9 @@
+package com.youssef.GridPulse.domain.fleet.dto;
+
+public record FleetInput(
+        String name,
+        String location,
+        String owner,
+        String description
+) {}
+
