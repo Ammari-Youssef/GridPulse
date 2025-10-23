29c73e5bc67a6f8ab369338b9415f0291ba280f6
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-10-24 00:40:15 +0100

    feat(security): Add security keys repositories

---
+++ src/main/java/com/youssef/GridPulse/domain/security/repository/SecurityKeyHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.youssef.GridPulse.domain.security.repository;
+
+import com.youssef.GridPulse.common.base.BaseHistoryRepository;
+import com.youssef.GridPulse.domain.security.entity.SecurityKeyHistory;
+
+import java.util.UUID;
+
+public interface SecurityKeyHistoryRepository extends BaseHistoryRepository<SecurityKeyHistory, UUID> {
+}
