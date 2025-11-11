8c5e4c741a03d565b46d05ec142c45303f812d3a
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-11-11 10:49:44 +0100

    feat(graphql): Update MessageInput and Message entities to use OffsetDateTime and JSONB for messageText in both java codebase and graphql schema

--- src/main/java/com/youssef/GridPulse/domain/message/dto/MessageInput.java
+++ src/main/java/com/youssef/GridPulse/domain/message/dto/MessageInput.java
@@ -4,19 +4,19 @@ import com.youssef.GridPulse.domain.message.enums.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public record MessageInput(
         @NotNull UUID deviceId,
         @NotBlank String cloudMessageId,
-        @NotBlank String messageText, // Raw JSON string
+        @NotBlank Object messageText, // Raw JSON string
         @NotNull MessageType messageType,
         @NotNull MessageFormat format,
         @NotNull MessageStatus status,
         @NotNull MessagePriority priority,
-        @NotNull Instant sentAt,
-        @NotNull Instant receivedAt
+        @NotNull OffsetDateTime sentAt,
+        @NotNull OffsetDateTime receivedAt
 ) {}
 
 
