a2cd03a2f9f70b5a7c6acda14dd175d6ab2ec2a6
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-11-13 00:08:36 +0100

    feat(pagination): Implement offset-based pagination for User and UserHistory queries; add PageResponse types

--- src/main/java/com/youssef/GridPulse/domain/identity/user/resolver/UserResolver.java
+++ src/main/java/com/youssef/GridPulse/domain/identity/user/resolver/UserResolver.java
@@ -1,10 +1,13 @@
 package com.youssef.GridPulse.domain.identity.user.resolver;
 
+import com.youssef.GridPulse.common.base.BaseResolver;
+import com.youssef.GridPulse.configuration.graphql.pagination.offsetBased.PageRequestInput;
+import com.youssef.GridPulse.configuration.graphql.pagination.offsetBased.PageResponse;
+import com.youssef.GridPulse.domain.identity.auth.dto.RegisterInput;
 import com.youssef.GridPulse.domain.identity.user.dto.UpdateUserInput;
 import com.youssef.GridPulse.domain.identity.user.entity.User;
 import com.youssef.GridPulse.domain.identity.user.entity.UserHistory;
 import com.youssef.GridPulse.domain.identity.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -12,55 +15,76 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Controller
-@RequiredArgsConstructor
-public class UserResolver {
+public class UserResolver extends BaseResolver<User, UserHistory, UUID, RegisterInput> {
 
-    private final UserService userService;
+    private final UserService service;
+
+    public UserResolver(UserService service) {
+        super(service);
+        this.service = service;
+    }
+
+    // Pagination Offset
+    @QueryMapping
+    public PageResponse<User> getAllUserPaged(@Argument PageRequestInput pageRequest) {
+        return super.getAllPaged(pageRequest);
+    }
+
+    @QueryMapping
+    public PageResponse<UserHistory> getAllUserHistoryPaged(@Argument PageRequestInput pageRequest) {
+        return super.getAllHistoryPaged(pageRequest);
+    }
+
+    @QueryMapping
+    public PageResponse<UserHistory> getUserHistoryByOriginalIdPaged(@Argument UUID originalId, @Argument PageRequestInput pageRequest) {
+        return super.getHistoryByOriginalIdPaged(originalId, pageRequest);
+    }
+
+    // Common CRUD
 
     @MutationMapping
     @PreAuthorize("hasRole('ADMIN')")
     public Boolean markUserHistorySynced(@Argument UUID id) {
-        return userService.markHistoryRecordAsSynced(id);
+        return super.markHistorySynced(id);
     }
 
     @QueryMapping(name = "getAllUsers")
     @PreAuthorize("hasRole('ADMIN')")
     public List<User> getAllUsers() {
-        return userService.getAllUsers();
+        return super.getAll();
     }
 
     @QueryMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public Optional<User> getUserById(@Argument UUID id) {
-        return userService.getUserById(id);
+    public User getUserById(@Argument UUID id) {
+        return super.getById(id);
     }
 
     @QueryMapping
     @PreAuthorize("hasRole('ADMIN')")
     public List<UserHistory> getUsersActivityHistory() {
-        return userService.getUsersActivityHistory();
+        return super.getAllHistory();
     }
 
     @MutationMapping
     @PreAuthorize("hasRole('ADMIN') or authentication.principal.id == #id")
     public User updateUser(@Argument UUID id, @Argument UpdateUserInput input) {
-        return userService.updateUser(id, input);
+        return service.updateUser(id, input);
     }
 
     @MutationMapping
     @PreAuthorize("hasRole('ADMIN')")
     public boolean deleteUser(@Argument UUID id) {
-       return userService.deleteUserById(id);
+       return super.delete(id);
     }
 
     @MutationMapping
     @PreAuthorize("hasRole('ADMIN')")
     public boolean toggleUserEnableStatus(@Argument UUID id) {
-       return userService.toggleUserEnableStatus(id);
+       return service.toggleUserEnableStatus(id);
     }
 
 }
