78856a4508e6906f23f73844fcb2202f51a23478
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-12-15 15:47:56 +0100

    refactor(structure): introduce core, shared and layout modules

---
+++ frontend/src/app/layout/layout.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShellComponent } from './shell/shell.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [NavbarComponent, ShellComponent, SidebarComponent],
+  exports: [ShellComponent, NavbarComponent, SidebarComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    // Angular Material Modules
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatSidenavModule,
+
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+  ],
+})
+export class LayoutModule {}
