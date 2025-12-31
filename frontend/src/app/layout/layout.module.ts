abec5ea1d4bdcbc036034f4dcede958438fe0731
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-12-31 17:58:08 +0100

    refactor(layout): Use MaterialModule in layout.module.ts

--- frontend/src/app/layout/layout.module.ts
+++ frontend/src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { NavbarComponent } from '@layout/navbar/navbar.component';
@@ -7,13 +8,7 @@ import { ShellComponent } from '@layout/shell/shell.component';
 import { SidebarComponent } from '@layout/sidebar/sidebar.component';
 
 // Angular Material
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterModule } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from '@shared/ui/material/material.module';
 
 
 @NgModule({
@@ -23,15 +18,9 @@ import { MatListModule } from '@angular/material/list';
     CommonModule,
     RouterModule,
 
-    // Angular Material Modules
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatSidenavModule,
+    // Angular Material Module  
+    MaterialModule,
 
-    MatListModule,
-    MatIconModule,
     RouterModule,
   ],
 })
