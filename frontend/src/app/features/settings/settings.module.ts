7f4f8f3bcce8b6bd849edf877894bb688883cc75
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-01-05 00:45:12 +0100

    feat(settings): add settings module

---
+++ frontend/src/app/features/settings/settings.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { NotificationSettingsComponent } from './notification-settings/notification-settings.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SecuritySettingsComponent } from './security-settings/security-settings.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@shared/shared.module';
+
+@NgModule({
+  declarations: [
+    SettingsComponent,
+    ProfileSettingsComponent,
+    SecuritySettingsComponent,
+    NotificationSettingsComponent,
+    UserManagementComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule,
+  ],
+})
+export class SettingsModule {}
+
