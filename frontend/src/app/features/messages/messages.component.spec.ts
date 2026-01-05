fa7a9a02b064d10283070e8baa2a46ac9da43d7e
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-01-06 00:18:38 +0100

    fix(message.spec): add declaration

--- frontend/src/app/features/messages/messages.component.spec.ts
+++ frontend/src/app/features/messages/messages.component.spec.ts
@@ -8,7 +8,8 @@ describe('MessagesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MessagesComponent]
+      imports: [],
+      declarations: [MessagesComponent],
     })
     .compileComponents();
 
