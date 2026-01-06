2b01180d8deab1b7887f6420ad1ed7c3876521c0
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-01-06 15:57:09 +0100

    feat(mock-apollo): add query field

--- frontend/src/app/testing/mock/apollo.mock.ts
+++ frontend/src/app/testing/mock/apollo.mock.ts
@@ -1,6 +1,7 @@
 import { of } from 'rxjs';
 
 export const ApolloMock = {
+  query: () => ({ valueChanges: of({ data: {} }) }),
   watchQuery: () => ({ valueChanges: of({ data: {} }) }),
   mutate: () => of({ data: {} }),
 };
